eb4b8e8c9b6b4848b62a994d50dddad916fdeecb
danimar-g <danielamar.gerard@gmail.com>
2024-06-04 18:35:56 -0400

    funciones total

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,24 +1,53 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  
+    let wordCount = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (i === 0 || text[i - 1] === ' ') {
+        wordCount++;
+      }
+    }
+    return wordCount;
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
     return text.length;
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const puntuación = /[.,;:!¿?¡()"¨¨\]{}<>'\-_]/g;
+    const nuevoTexto = text.replace(puntuación, "");
+    return nuevoTexto.replace(/\s+/g, '').length;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    let TotalPalabras = text.split(" ");
+    let TotalCaracteres = text.replace(/\s+/g, "").length;
+    let Promedio = TotalCaracteres / TotalPalabras.length;
+    return Promedio;
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    console.log(text.match(/\d+/g));
+    return text.match(/\d+/g).length;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    return text.match("/\d+/g").length;
   },
+  getNumberSum: (text) => {
+    let palabras = text.split(' ');
+    let suma = 0;
+    palabras.forEach(palabra => {
+      if (!isNaN(palabra) && palabra.trim() !== "") {
+        suma += parseInt(palabra);
+        console.log(suma)
+      }
+    })
+    return suma;
+  }
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,45 @@ textarea.addEventListener('keyup', function (e) {
     let charactercount = analyzer.getCharacterCount(e.target.value);
     console.log(charactercount);
     document.getElementsByClassName("item1")[0].innerHTML = "Caracteres:" + charactercount;
+});
+textarea.addEventListener('keyup', function (e) {
+    let totalCaracteresSinEspacios = analyzer.getCharacterCountExcludingSpaces(e.target.value);
+    console.log(totalCaracteresSinEspacios);
+    document.getElementsByClassName("item2")[0].innerHTML = "Sin espacio:" + totalCaracteresSinEspacios;
+})
 
+textarea.addEventListener('keyup', function (e) {
+    let TotalPalabras = analyzer.getWordCount(e.target.value);
+    console.log(TotalPalabras);
+    document.getElementsByClassName("item3")[0].innerHTML = "Palabras:" + TotalPalabras;
+})
 
-    
-}); 
+textarea.addEventListener('keyup', function (e) {
+    let Numeros = analyzer.getNumberCount(e.target.value);
+    console.log(Numeros);
+    document.getElementsByClassName("item4")[0].innerHTML = "Números:" + Numeros;
+})
+
+textarea.addEventListener('keyup', function (e) {
+    let SumaNumeros = analyzer.getNumberSum(e.target.value);
+    console.log(SumaNumeros);
+    document.getElementsByClassName("item5")[0].innerHTML = "Suma Números:" + SumaNumeros;
+})
+
+textarea.addEventListener('keyup', function (e) {
+    let PromedioLongitud = analyzer.getAverageWordLength(e.target.value);
+    console.log(PromedioLongitud);
+    document.getElementsByClassName("item6")[0].innerHTML = "Promedio Longitud:" + PromedioLongitud;
+})
+const resetbutton = document.getElementById("reset-button")
+let userinput = document.getElementsByName("user-input")[0];
+function limpiar() {
+    userinput.value = ""; //limpia el contenido del cuadro de texto
+}
+resetbutton.addEventListener('click', limpiar);
+
+//function hola(nombre) { console.log("hola " + nombre); }
+const hola = (nombre) =>{ console.log("hola " + nombre); };
+hola("daniela"); 
+hola ("pablo");
+
